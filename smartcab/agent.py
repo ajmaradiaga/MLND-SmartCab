536a61dd6ea4da03f2fbd4584fda8beb480403e3
Antonio Maradiaga <me@ajmaradiaga.com>
2017-05-30 16:56:44 +0100

    Modified agent.py to accept parameters from command line

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -3,6 +3,22 @@ import math
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+import sys, getopt
+from collections import OrderedDict 
+
+args_dict = OrderedDict()
+args_dict["learning"] = False
+args_dict["alpha"] = 0.5
+args_dict["epsilon"] = 1.0
+args_dict["enforce_deadline"] = False
+args_dict["update_delay"] = 0.01
+args_dict["display"] = False 
+args_dict["log_metrics"] = True 
+args_dict["optimized"] = False
+args_dict["n_test"] = 10 
+args_dict["tolerance"]= 0.05
+args_dict["decay_fx"]= "None"
+args_dict["params_output"]= ""
 
 class LearningAgent(Agent):
     """ An agent that learns to drive in the Smartcab world.
@@ -23,6 +39,9 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
+        self.trial_num = 0.0
+        
+        random.seed(888) 
 
 
     def reset(self, destination=None, testing=False):
@@ -39,6 +58,37 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
+        
+        if testing:
+            self.epsilon = 0.0
+            self.alpha = 0.0
+        else:
+            #bad actions, major violation, major accident, rolling avg reward per action, reliability
+            self.trial_num += 1.0
+            
+            if args_dict["decay_fx"] == "initial":
+                self.epsilon = self.epsilon - 0.05
+            elif args_dict["decay_fx"] == "oneovertp2":
+                self.epsilon = 1 / (self.trial_num ** 2)
+            elif args_dict["decay_fx"] == "expat":
+                self.epsilon = math.exp(-self.alpha * self.trial_num)
+            elif args_dict["decay_fx"] == "cosat":
+                self.epsilon = math.cos(self.alpha * self.trial_num)
+            else:
+                self.epsilon = 0.0
+            #self.epsilon = 1 / (self.trial_num ** 2)
+            
+            #0.001, 0.001, 0.001, 1.5, 80 peak 90
+            #alpha=0.01, epsilon=1.0, tolerance=0.01
+            
+            #0.035, 0.0212, 0.01, 0.9, 40 peak 90
+            #alpha=0.05, epsilon=1.0, tolerance=0.01
+            
+            
+            #0.1711, 0.1283, 0.0428, -1.8, 40 peak 40
+            #alpha=0.05, epsilon=1.0, tolerance=0.01
+            #
+        
 
         return None
 
@@ -55,8 +105,8 @@ class LearningAgent(Agent):
         ########### 
         ## TO DO ##
         ###########
-        # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+        # Set 'state' as a tuple of relevant data for the agent
+        state = (inputs["light"], inputs["oncoming"], inputs["left"], inputs["right"], waypoint)
 
         return state
 
@@ -69,8 +119,12 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Calculate the maximum Q-value of all actions for a given state
-
-        maxQ = None
+        maxQ = -8888.8
+        q_state = self.Q[state]
+        
+        for action, value in q_state.iteritems():
+            if maxQ < value:
+                maxQ = value
 
         return maxQ 
 
@@ -84,7 +138,9 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-
+        if self.learning == True and state not in self.Q:
+            self.Q[state] = {action : 0.0 for action in self.valid_actions }
+            
         return
 
 
@@ -95,14 +151,23 @@ class LearningAgent(Agent):
         # Set the agent state and default action
         self.state = state
         self.next_waypoint = self.planner.next_waypoint()
-        action = random.choice(self.valid_actions)
-
+        
         ########### 
         ## TO DO ##
         ###########
         # When not learning, choose a random action
         # When learning, choose a random action with 'epsilon' probability
         #   Otherwise, choose an action with the highest Q-value for the current state
+        
+        if self.learning == True:
+            if random.random() < self.epsilon:
+                action = random.choice(self.valid_actions)
+            else:
+                maxQ = self.get_maxQ(state)
+                action = random.choice([k for k,v in self.Q[state].iteritems() if v == maxQ])
+                            
+        else:
+            action = random.choice(self.valid_actions)
  
         return action
 
@@ -117,6 +182,8 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
+        if self.learning == True:
+            self.Q[state][action] += (self.alpha * reward)
 
         return
 
@@ -133,12 +200,20 @@ class LearningAgent(Agent):
         self.learn(state, action, reward)   # Q-learn
 
         return
-        
 
-def run():
+def run(argv):
     """ Driving function for running the simulation. 
         Press ESC to close the simulation, or [SPACE] to pause the simulation. """
 
+    args_dict = parse_commands(argv)
+    
+    if len(args_dict["params_output"]) > 0:
+        f = open(args_dict["params_output"], 'wb')
+        for k,v in args_dict.iteritems():
+            f.write("{} = {} {}\n".format(k, v, type(v)))
+        f.close() 
+    
+    
     ##############
     # Create the environment
     # Flags:
@@ -153,13 +228,16 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent, \
+                             learning=args_dict["learning"], \
+                             alpha=args_dict["alpha"], \
+                             epsilon=args_dict["epsilon"])
     
     ##############
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent, enforce_deadline=True)
+    env.set_primary_agent(agent, enforce_deadline=args_dict["enforce_deadline"])
 
     ##############
     # Create the simulation
@@ -168,15 +246,51 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True)
+    sim = Simulator(env, \
+                    update_delay=args_dict["update_delay"], \
+                    log_metrics=args_dict["log_metrics"], \
+                    optimized=args_dict["optimized"], \
+                    display=args_dict["display"])
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=args_dict["n_test"], \
+            tolerance=args_dict["tolerance"])
 
+def parse_commands(argv):
+    
+    
+    help_string = ""
+    default_string = "\n\nDefault values:\n"
+    for k, v in args_dict.iteritems():
+        help_string += "--" + k + " " + str(type(v)) + " "
+        default_string += "--"+k + " " + str(v) + " \n"
+    
+    help_string += default_string
+    
+    try:
+        if len(argv) == 0:
+            print("No parameters defined.")
+            return args_dict
+        opts, args = getopt.getopt(argv,"hl:a:e:ud:d:lm:op:nt:tol",["help"] + [k + "=" for k in args_dict])
+    except getopt.GetoptError:
+        print 'agent.py ' + help_string
+        sys.exit(2)
+    for opt, arg in opts:
+        opt = opt.replace("--", "")
+        # print(opt, arg)
+        if opt in ("-h", '--help'):
+            print 'agent.py ' + help_string
+            sys.exit()
+        elif type(args_dict[opt]) == bool:
+            args_dict[opt] = True if arg.lower() in ("yes", "true", "t", "1") else False
+        elif opt in args_dict:
+            args_dict[opt] = type(args_dict[opt])(arg)
+        
+    return args_dict
 
 if __name__ == '__main__':
-    run()
+    run(sys.argv[1:])
